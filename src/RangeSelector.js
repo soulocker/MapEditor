da17ec228b5f01313c45372c7ed6569f06255fd0
soulocker <soulocker@msn.com>
2023-03-23 15:32:21 +0800

    修正了地编布怪模式如果不是从左上角开始拖拽产生的坐标错误

--- src/RangeSelector.js
+++ src/RangeSelector.js
@@ -2,129 +2,143 @@
  * Created by Administrator on 2017/4/24.
  */
 
-
 var RangeSelector = function () {
-  this._callbacks = [];
-  this._rect = new Rectangle(0, 0, 0, 0);
+    this._callbacks = [];
+    this._rect = new Rectangle(0, 0, 0, 0);
 
-  this._offsetX = $("#terrainSelect").width();
-  this._offsetY = 36;
+    this._offsetX = $("#terrainSelect").width();
+    this._offsetY = 36;
 
+    let div = "<div id='border'></div>";
+    let _map = $("#map");
+    _map.append(div);
+    $("#border").css({
+        position: "absolute",
+    });
 
-  let div = "<div id='border'></div>";
-  let _map = $("#map");
-  _map.append(div);
-  $("#border").css({
-    position: "absolute",
-  });
-
-  _map.mousedown(this._mouseDownHandler.bind(this));
-  _map.mouseup(this._mouseUpHandler.bind(this));
+    _map.mousedown(this._mouseDownHandler.bind(this));
+    _map.mouseup(this._mouseUpHandler.bind(this));
 };
 
 RangeSelector.prototype = {
-  constructor: RangeSelector,
+    constructor: RangeSelector,
 
-  addCallback: function (callback) {
-    if (this._callbacks.indexOf(callback) !== -1) return;
-    this._callbacks.push(callback);
-  },
+    addCallback: function (callback) {
+        if (this._callbacks.indexOf(callback) !== -1) return;
+        this._callbacks.push(callback);
+    },
 
-  removeCallback: function (callback) {
-    let index = this._callbacks.indexOf(callback);
-    if (index === -1) return;
-    this._callbacks.splice(index, 1);
-  },
+    removeCallback: function (callback) {
+        let index = this._callbacks.indexOf(callback);
+        if (index === -1) return;
+        this._callbacks.splice(index, 1);
+    },
 
-  _mouseDownHandler: function (event) {
-    if (event.button === 0) {
-      let _map = $("#map");
-      // 按下左键时鼠标在画布上的坐标
-      this.startX = event.clientX - this._offsetX + _map.scrollLeft();
-      this.startY = event.clientY - this._offsetY + _map.scrollTop();
+    _mouseDownHandler: function (event) {
+        if (event.button === 0) {
+            let _map = $("#map");
+            // 按下左键时鼠标在画布上的坐标
+            this.startX = event.clientX - this._offsetX + _map.scrollLeft();
+            this.startY = event.clientY - this._offsetY + _map.scrollTop();
 
-      this._startX = parseInt(event.offsetX / GRID_WIDTH);
-      this._startY = parseInt(event.offsetY / GRID_HEIGHT);
-      this.endX = this.startX;
-      this.endY = this.startY;
+            this._startX = parseInt(event.offsetX / GRID_WIDTH);
+            this._startY = parseInt(event.offsetY / GRID_HEIGHT);
+            this.endX = this.startX;
+            this.endY = this.startY;
 
-      $("#border").css({
-        left: this.startX + "px",
-        top: this.startY + "px",
-        border: "1px solid  #0000ff",
-        width: 0,
-        height: 0,
-        display: "block",
-      });
-      _map.mousemove(this._mouseMoveHandler.bind(this));
-      this.draw();
-    }
-  },
+            $("#border").css({
+                left: this.startX + "px",
+                top: this.startY + "px",
+                border: "1px solid  #0000ff",
+                width: 0,
+                height: 0,
+                display: "block",
+            });
+            _map.mousemove(this._mouseMoveHandler.bind(this));
+            this.draw();
+        }
+    },
 
-  _mouseMoveHandler: function (event) {
-    let _mapZone = $("#map");
-    this.endX = event.clientX - this._offsetX + _mapZone.scrollLeft();
-    this.endY = event.clientY - this._offsetY + _mapZone.scrollTop();
-    this.draw();
-  },
+    _mouseMoveHandler: function (event) {
+        let _mapZone = $("#map");
+        this.endX = event.clientX - this._offsetX + _mapZone.scrollLeft();
+        this.endY = event.clientY - this._offsetY + _mapZone.scrollTop();
+        this.draw();
+    },
 
-  _mouseUpHandler: function (event) {
-    if (Mode === MapEditorMode.View) {
-      let endX = parseInt(event.offsetX / GRID_WIDTH);
-      let endY = parseInt(event.offsetY / GRID_HEIGHT);
-      console.log("起点 X:" + this._startX + " | 起点 Y:" + this._startY);
-      console.log("终点 X:" + endX + " | 终点 Y:" + endY);
+    _mouseUpHandler: function (event) {
+        if (Mode === MapEditorMode.View) {
+            let endX = parseInt(event.offsetX / GRID_WIDTH);
+            let endY = parseInt(event.offsetY / GRID_HEIGHT);
+            console.log("起点 X:" + this._startX + " | 起点 Y:" + this._startY);
+            console.log("终点 X:" + endX + " | 终点 Y:" + endY);
 
-      let width = endX - this._startX + 1;
-      let height = endY - this._startY + 1;
-      let copyTxt =
-        this._startX + "\t" + this._startY + "\t" + width + "\t" + height;
-      navigator.clipboard.writeText(copyTxt).then(() => {
-        console.log("复制：" + copyTxt);
-      });
-    }
-    $("#border").css({
-      border: "0px solid  #0000ff",
-      width: 0,
-      height: 0,
-      display: "none",
-    });
-    $("#map").unbind("mousemove");
-    for (const element of this._callbacks) {
-      element(this);
-    }
-  },
+            let width = Math.abs(endX - this._startX) + 1;
+            let height = Math.abs(endY - this._startY) + 1;
+            let copyTxt =
+                Math.min(this._startX, endX) +
+                "\t" +
+                Math.min(this._startY, endY) +
+                "\t" +
+                width +
+                "\t" +
+                height;
+            navigator.clipboard.writeText(copyTxt).then(() => {
+                console.log("复制：" + copyTxt);
+            });
+        }
+        $("#border").css({
+            border: "0px solid  #0000ff",
+            width: 0,
+            height: 0,
+            display: "none",
+        });
+        $("#map").unbind("mousemove");
+        for (const element of this._callbacks) {
+            element(this);
+        }
+    },
 
-  getRect: function () {
-    return this._rect;
-  },
+    getRect: function () {
+        return this._rect;
+    },
 
-  draw: function () {
-    var minX, maxX, minY, maxY;
-    console.log("startX:" + this.startX + " | startY:" + this.startY + "|" + "endX:" + this.endX + " | endY:" + this.endY);
-    if (this.startX <= this.endX) {
-      minX = this.startX;
-      maxX = this.endX;
-    } else {
-      minX = this.endX;
-      maxX = this.startX;
-    }
-    if (this.startY <= this.endY) {
-      minY = this.startY;
-      maxY = this.endY;
-    } else {
-      minY = this.endY;
-      maxY = this.startY;
-    }
-    $("#border").css({
-      left: minX + "px",
-      top: minY + "px",
-      width: (maxX - minX) + "px",
-      height: (maxY - minY) + "px",
-    });
-    this._rect.x = minX;
-    this._rect.y = minY;
-    this._rect.width = maxX - minX;
-    this._rect.height = maxY - minY;
-  },
+    draw: function () {
+        var minX, maxX, minY, maxY;
+        console.log(
+            "startX:" +
+                this.startX +
+                " | startY:" +
+                this.startY +
+                "|" +
+                "endX:" +
+                this.endX +
+                " | endY:" +
+                this.endY
+        );
+        if (this.startX <= this.endX) {
+            minX = this.startX;
+            maxX = this.endX;
+        } else {
+            minX = this.endX;
+            maxX = this.startX;
+        }
+        if (this.startY <= this.endY) {
+            minY = this.startY;
+            maxY = this.endY;
+        } else {
+            minY = this.endY;
+            maxY = this.startY;
+        }
+        $("#border").css({
+            left: minX + "px",
+            top: minY + "px",
+            width: maxX - minX + "px",
+            height: maxY - minY + "px",
+        });
+        this._rect.x = minX;
+        this._rect.y = minY;
+        this._rect.width = maxX - minX;
+        this._rect.height = maxY - minY;
+    },
 };
