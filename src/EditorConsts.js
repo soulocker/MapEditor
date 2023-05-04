5ff944ffe90fffdc5edd8e005a455638f1797302
soulocker <soulocker@msn.com>
2023-05-04 16:31:19 +0800

    日常修改归档

--- src/EditorConsts.js
+++ src/EditorConsts.js
@@ -2,45 +2,41 @@
  * Created by Administrator on 2017/4/24.
  */
 if (!Object.assign) {
-  // 定义assign方法
-  Object.defineProperty(Object, "assign", {
-    enumerable: false,
-    configurable: true,
-    writable: true,
-    value: function (target) {
-      // assign方法的第一个参数
-      "use strict";
-      // 第一个参数为空，则抛错
-      if (target === undefined || target === null) {
-        throw new TypeError("Cannot convert first argument to object");
-      }
-      let to = Object(target);
-      // 遍历剩余所有参数
-      for (let i = 1; i < arguments.length; i++) {
-        let nextSource = arguments[i];
-        // 参数为空，则跳过，继续下一个
-        if (nextSource === undefined || nextSource === null) {
-          continue;
+    // 定义assign方法
+    Object.defineProperty(Object, "assign", {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (target) {
+            // assign方法的第一个参数
+            "use strict";
+            // 第一个参数为空，则抛错
+            if (target === undefined || target === null) {
+                throw new TypeError("Cannot convert first argument to object");
+            }
+            let to = Object(target);
+            // 遍历剩余所有参数
+            for (let i = 1; i < arguments.length; i++) {
+                let nextSource = arguments[i];
+                // 参数为空，则跳过，继续下一个
+                if (nextSource === undefined || nextSource === null) {
+                    continue;
+                }
+                nextSource = Object(nextSource);
+                // 获取改参数的所有key值，并遍历
+                let keysArray = Object.keys(nextSource);
+                for (let nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    let nextKey = keysArray[nextIndex];
+                    let desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    // 如果不为空且可枚举，则直接浅拷贝赋值
+                    if (desc !== undefined && desc.enumerable) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+            return to;
         }
-        nextSource = Object(nextSource);
-        // 获取改参数的所有key值，并遍历
-        let keysArray = Object.keys(nextSource);
-        for (
-          let nextIndex = 0, len = keysArray.length;
-          nextIndex < len;
-          nextIndex++
-        ) {
-          let nextKey = keysArray[nextIndex];
-          let desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-          // 如果不为空且可枚举，则直接浅拷贝赋值
-          if (desc !== undefined && desc.enumerable) {
-            to[nextKey] = nextSource[nextKey];
-          }
-        }
-      }
-      return to;
-    },
-  });
+    });
 }
 var MapEditorMode = { Edit: 0, View: 1 };
 // ----------------------------------------------------------------
@@ -48,14 +44,17 @@ var MapEditorMode = { Edit: 0, View: 1 };
 // ----------------------------------------------------------------
 let gridWidthValueControl = document.getElementById("gridWidth");
 let gridHeightValueControl = document.getElementById("gridHeight");
-gridWidthValueControl.addEventListener("change", function (e) {GRID_WIDTH = e.target.value;});
-gridHeightValueControl.addEventListener("change", function (e) { GRID_HEIGHT = e.target.value; });
+gridWidthValueControl.addEventListener("change", function (e) {
+    GRID_WIDTH = e.target.value;
+});
+gridHeightValueControl.addEventListener("change", function (e) {
+    GRID_HEIGHT = e.target.value;
+});
 
 //格子宽度
 var GRID_WIDTH = parseInt(gridWidthValueControl.value);
 //格子高度
 var GRID_HEIGHT = parseInt(gridHeightValueControl.value);
-
 
 var MAP_TILE_WIDTH = $("#tileWidth").val();
 var MAP_TILE_HEIGHT = $("#tileHeight").val();
@@ -94,11 +93,11 @@ var MapWidth, MapHeight;
 var MapName;
 
 function getValidWidth(width) {
-  return Math.ceil(width / GRID_WIDTH) * GRID_WIDTH;
+    return Math.ceil(width / GRID_WIDTH) * GRID_WIDTH;
 }
 
 function getValidHeight(height) {
-  return Math.ceil(height / GRID_HEIGHT) * GRID_HEIGHT;
+    return Math.ceil(height / GRID_HEIGHT) * GRID_HEIGHT;
 }
 
-ViewportScale = 1
+ViewportScale = 1;
--- src/main.js
+++ src/main.js
@@ -1,135 +1,128 @@
 $(document).ready(function () {
-  window.URL = window.URL || window.webkitURL;
-  window.BlobBuilder =
-    window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+    window.URL = window.URL || window.webkitURL;
+    window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
 
-  //菜单控制
-  new MenuFile();
-  new MenuConfig();
+    //菜单控制
+    new MenuFile();
+    new MenuConfig();
 
-  MapWidth = $("#mapImg").width();
-  MapHeight = $("#mapImg").height();
+    MapWidth = $("#mapImg").width();
+    MapHeight = $("#mapImg").height();
 
-  var w = getValidWidth(MapWidth);
-  var h = getValidHeight(MapHeight);
+    var w = getValidWidth(MapWidth);
+    var h = getValidHeight(MapHeight);
 
-  MapName = "HowtoUse.png";
+    MapName = "HowtoUse.png";
 
-  //范围选择器
-  var selector = new RangeSelector();
+    //范围选择器
+    var selector = new RangeSelector();
 
-  areaLayer = new AreaLayer("areaLayer", selector, w, h);
-  terrainLayer = new TerrainLayer("terrainLayer", selector, w, h);
+    areaLayer = new AreaLayer("areaLayer", selector, w, h);
+    terrainLayer = new TerrainLayer("terrainLayer", selector, w, h);
 
-  areaLayer.setEnable(false);
-  terrainLayer.setEnable(true);
+    areaLayer.setEnable(false);
+    terrainLayer.setEnable(true);
 
-  areaLayer.isClear = false;
-  terrainLayer.isClear = false;
+    areaLayer.isClear = false;
+    terrainLayer.isClear = false;
 
-  let KEY_ZOOM_IN;
-  let KEY_ZOOM_OUT;
-  let KEY_BRUSH1 = 49;  // 1
-  let KEY_BRUSH2 = 50;  // 2
-  let KEY_ERASER = 18;  // ALT
+    let KEY_ZOOM_IN;
+    let KEY_ZOOM_OUT;
+    let KEY_BRUSH1 = 49; // 1
+    let KEY_BRUSH2 = 50; // 2
+    let KEY_ERASER = 18; // ALT
 
+    $(document).keydown(function (event) {
+        let gridTypeCheckboxGroup = $("#terrainSelect input[name='terrainLayer']");
+        switch (event.keyCode) {
+            case KEY_ERASER:
+                areaLayer.setEnable(false);
+                terrainLayer.setEnable(false);
 
-  $(document).keydown(function (event) {
-    let gridTypeCheckboxGroup = $("#terrainSelect input[name='terrainLayer']");
-    switch (event.keyCode) {
-      case KEY_ERASER:
-        areaLayer.setEnable(false);
-        terrainLayer.setEnable(false);
+                areaLayer.isClear = false;
+                terrainLayer.isClear = true;
+                break;
+            case KEY_BRUSH1:
+                switchCheckboxStatus(gridTypeCheckboxGroup[0], true);
+                switchCheckboxStatus(gridTypeCheckboxGroup[1], false);
+                break;
+            case KEY_BRUSH2:
+                switchCheckboxStatus(gridTypeCheckboxGroup[1], true);
+                switchCheckboxStatus(gridTypeCheckboxGroup[0], false);
+                break;
+            default:
+                break;
+        }
+    });
 
-        areaLayer.isClear = false;
-        terrainLayer.isClear = true;
-        break;
-      case KEY_BRUSH1:
-        switchCheckboxStatus(gridTypeCheckboxGroup[0], true);
-        switchCheckboxStatus(gridTypeCheckboxGroup[1], false);
-        break;
-      case KEY_BRUSH2:
-        switchCheckboxStatus(gridTypeCheckboxGroup[1], true);
-        switchCheckboxStatus(gridTypeCheckboxGroup[0], false);
-        break;
-      default:
-        break;
-    }
-  });
+    $(document).keyup(function (event) {
+        if (event.keyCode === 18) {
+            areaLayer.setEnable(false);
+            terrainLayer.setEnable(true);
 
-  $(document).keyup(function (event) {
-    if (event.keyCode === 18) {
-      areaLayer.setEnable(false);
-      terrainLayer.setEnable(true);
+            areaLayer.isClear = false;
+            terrainLayer.isClear = false;
+        }
+    });
 
-      areaLayer.isClear = false;
-      terrainLayer.isClear = false;
-    }
-  });
+    //地表编辑设置
+    $("#terrainSelect input[name='editorType']").change(function () {
+        if ($(this).val() === "0") {
+            areaLayer.setEnable(true);
+            terrainLayer.setEnable(false);
 
-  //地表编辑设置
-  $("#terrainSelect input[name='editorType']").change(function () {
-    if ($(this).val() === "0") {
-      areaLayer.setEnable(true);
-      terrainLayer.setEnable(false);
+            areaLayer.isClear = false;
+            terrainLayer.isClear = false;
+        } else if ($(this).val() === "1") {
+            areaLayer.setEnable(false);
+            terrainLayer.setEnable(true);
 
-      areaLayer.isClear = false;
-      terrainLayer.isClear = false;
-    } else if ($(this).val() === "1") {
-      areaLayer.setEnable(false);
-      terrainLayer.setEnable(true);
+            areaLayer.isClear = false;
+            terrainLayer.isClear = false;
+        } else {
+            areaLayer.setEnable(false);
+            terrainLayer.setEnable(false);
 
-      areaLayer.isClear = false;
-      terrainLayer.isClear = false;
-    } else {
-      areaLayer.setEnable(false);
-      terrainLayer.setEnable(false);
+            if ($(this).val() === "2") {
+                areaLayer.isClear = true;
+                terrainLayer.isClear = false;
+            } else {
+                areaLayer.isClear = false;
+                terrainLayer.isClear = true;
+            }
+        }
+    });
 
-      if ($(this).val() === "2") {
-        areaLayer.isClear = true;
-        terrainLayer.isClear = false;
-      } else {
-        areaLayer.isClear = false;
-        terrainLayer.isClear = true;
-      }
-    }
-  });
+    let sceneXElement = $("#sceneX");
+    let sceneYElement = $("#sceneY");
 
-  let sceneXElement = $("#sceneX");
-  let sceneYElement = $("#sceneY");
+    $("#terrainLayer").mousemove(function (event) {
+        sceneXElement.text(event.offsetX + "(" + parseInt(event.offsetX / GRID_WIDTH) + ")");
+        sceneYElement.text(event.offsetY + "(" + parseInt(event.offsetY / GRID_HEIGHT) + ")");
+    });
 
-  $("#terrainLayer").mousemove(function (event) {
-    sceneXElement.text(
-      event.offsetX + "(" + parseInt(event.offsetX / GRID_WIDTH) + ")"
-    );
-    sceneYElement.text(
-      event.offsetY + "(" + parseInt(event.offsetY / GRID_HEIGHT) + ")"
-    );
-  });
+    $("#areaAlphaSet").slider({
+        formatter: function (value) {
+            areaLayer && areaLayer.setOpacity(value);
 
-  $("#areaAlphaSet").slider({
-    formatter: function (value) {
-      areaLayer && areaLayer.setOpacity(value);
+            return value;
+        }
+    });
 
-      return value;
-    },
-  });
+    $("#terrainAlphaSet").slider({
+        formatter: function (value) {
+            terrainLayer && terrainLayer.setOpacity(value);
 
-  $("#terrainAlphaSet").slider({
-    formatter: function (value) {
-      terrainLayer && terrainLayer.setOpacity(value);
+            return value;
+        }
+    });
 
-      return value;
-    },
-  });
-
-  $("#mapBrightness").slider({
-    formatter: function (value) {
-      document.getElementById("mapImg").style.filter =
-        "brightness(" + value + ")";
-      return value;
-    },
-  });
+    $("#mapBrightness").slider({
+        formatter: function (value) {
+            document.getElementById("mapImg").style.filter = "brightness(" + value + ")";
+            return value;
+        }
+    });
 });
 
 /*
@@ -139,13 +132,10 @@ $(document).ready(function () {
  * @return {void}
  * */
 function switchCheckboxStatus(checkbox, mode) {
-  if (
-    (mode === undefined && checkbox.hasAttribute("checked")) ||
-    mode === false
-  ) {
-    checkbox.removeAttribute("checked");
-  } else {
-    let x = document.createAttribute("checked");
-    checkbox.attributes.setNamedItem(x);
-  }
+    if ((mode === undefined && checkbox.hasAttribute("checked")) || mode === false) {
+        checkbox.removeAttribute("checked");
+    } else {
+        let x = document.createAttribute("checked");
+        checkbox.attributes.setNamedItem(x);
+    }
 }
